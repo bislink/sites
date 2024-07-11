36dac6570151971df8158f6aad3364a3e19c160d
ns21u2204 <ns21u2204@ns21.a1z.us>
2024-07-10 20:02:24 -0500

    001 ready, kidna

--- lib/Sites/Controller/Example.pm
+++ lib/Sites/Controller/Example.pm
@@ -4,8 +4,21 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 # This action will render a template
 sub welcome ($self) {
 
+my @SITES=("a1z.us biz-land.com bizzland.com bislinks.com a2z.red books66.com biblewhat.com
+bizzland.in bizland.uk bizland.asia preachword.com books66.com mroshni.com msneha.com
+sumu.uk sumu.in sirsumu.com sumu.name sumu.dev biz-land.in ");
+
+my $out = qq{<table class="table table-striped table-compact">}; 
+
+for my $SITE (sort @SITES) {
+  my $dig = `dig +short $SITE`;
+  $out .= qq{<tr> <td>$SITE</td> <td>$dig</td> </tr> }; 
+}
+
+$out .= qq{</table>};
+
   # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+  $self->render(msg => 'Sites!', out => $out);
 }
 
 1;

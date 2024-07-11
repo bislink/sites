11600aae08cd30bcd0964308d95dd79b77bb96f6
ns21u2204 <ns21u2204@ns21.a1z.us>
2024-07-10 20:24:57 -0500

    002 Ready: templates, main page, css, etc.

--- lib/Sites/Controller/Example.pm
+++ lib/Sites/Controller/Example.pm
@@ -4,15 +4,17 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 # This action will render a template
 sub welcome ($self) {
 
-my @SITES=("a1z.us biz-land.com bizzland.com bislinks.com a2z.red books66.com biblewhat.com
-bizzland.in bizland.uk bizland.asia preachword.com books66.com mroshni.com msneha.com
-sumu.uk sumu.in sirsumu.com sumu.name sumu.dev biz-land.in ");
+my @SITES=('a1z.us', 'biz-land.com', 'bizzland.com', 'bislinks.com', 'a2z.red', 'books66.com', 'biblewhat.com', 
+'bizzland.in', 'bizland.uk', 'bizland.asia', 'preachword.com', 'books66.com', 'mroshni.com', 'msneha.com', 
+'sumu.uk', 'sumu.in', 'sirsumu.com', 'sumu.name', 'sumu.dev', 'biz-land.in');
 
-my $out = qq{<table class="table table-striped table-compact">}; 
+my $out = qq{<table class="table table-striped table-responsive table-hover">}; 
 
-for my $SITE (sort @SITES) {
+my $serial = '0';
+foreach my $SITE (sort @SITES) {
+  $serial++ if $SITE;
   my $dig = `dig +short $SITE`;
-  $out .= qq{<tr> <td>$SITE</td> <td>$dig</td> </tr> }; 
+  $out .= qq{<tr> <td>$serial</td> <td>$SITE</td> <td>$dig</td> </tr> }; 
 }
 
 $out .= qq{</table>};
